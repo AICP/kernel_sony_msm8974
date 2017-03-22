cf26b9b278bd55e92f039e4f39963d48f13b8daf
Abdulla Anam <abdullahanam@codeaurora.org>
2017-03-22 14:00:10 +0530

    msm: vidc: Protect debug_buffer access in core_info_read with lock.
    Serialize core_info_read with lock so that multiple concurrent
    threads do not cause the write to overflow. Also have the bound
    check to avoid overflow in write_str function.
    
    CRs-Fixed: 2013361
    Change-Id: Ia18a4b94cafd69af1d367861f2499fc202f18e9f
    Signed-off-by: Abdulla Anam <abdullahanam@codeaurora.org>
    Signed-off-by: Sanjay Singh <sisanj@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
+++ drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
@@ -616,6 +616,7 @@ static int __init msm_vidc_init(void)
 
 	INIT_LIST_HEAD(&vidc_driver->cores);
 	mutex_init(&vidc_driver->lock);
+	msm_vidc_debugfs_init_drv();
 	vidc_driver->debugfs_root = debugfs_create_dir("msm_vidc", NULL);
 	if (!vidc_driver->debugfs_root)
 		dprintk(VIDC_ERR,
@@ -625,6 +626,8 @@ static int __init msm_vidc_init(void)
 	if (rc) {
 		dprintk(VIDC_ERR,
 			"Failed to register platform driver\n");
+		msm_vidc_debugfs_deinit_drv();
+		debugfs_remove_recursive(vidc_driver->debugfs_root);
 		kfree(vidc_driver);
 		vidc_driver = NULL;
 	}
@@ -635,6 +638,7 @@ static int __init msm_vidc_init(void)
 static void __exit msm_vidc_exit(void)
 {
 	platform_driver_unregister(&msm_vidc_driver);
+	msm_vidc_debugfs_deinit_drv();
 	debugfs_remove_recursive(vidc_driver->debugfs_root);
 	kfree(vidc_driver);
 	vidc_driver = NULL;
