e5aaa69e0eee7c2876e2dfa4ff80c14f1cd4d855
munjeni <mystopspammail@example.com>
2017-12-01 18:51:57 +0100

    Update amami device configuration
    Change-Id: Ie76bf43d7369aa62d609524da97862e5ed69f9ae

--- sound/soc/msm/msm8974.c
+++ sound/soc/msm/msm8974.c
@@ -349,6 +349,7 @@ static int msm8974_liquid_ext_spk_power_amp_init(void)
 	return 0;
 }
 
+#if !defined(CONFIG_MACH_SONY_SHINANO) && !defined(CONFIG_MACH_SONY_RHINE)
 static void msm8974_liquid_ext_ult_spk_power_amp_enable(u32 on)
 {
 	if (on) {
@@ -370,6 +371,7 @@ static void msm8974_liquid_ext_ult_spk_power_amp_enable(u32 on)
 	pr_debug("%s: %s external ultrasound SPKR_DRV PAs.\n", __func__,
 			on ? "Enable" : "Disable");
 }
+#endif
 
 static void msm8974_liquid_ext_spk_power_amp_enable(u32 on)
 {
@@ -681,7 +683,7 @@ static int msm_ext_spkramp_event(struct snd_soc_dapm_widget *w,
 
 }
 
-#ifndef CONFIG_MACH_SONY_SHINANO
+#if !defined(CONFIG_MACH_SONY_SHINANO) && !defined(CONFIG_MACH_SONY_RHINE)
 static int msm_ext_spkramp_ultrasound_event(struct snd_soc_dapm_widget *w,
 			     struct snd_kcontrol *k, int event)
 {
